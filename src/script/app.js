62d6fcae366b2a7c3880df593f6342c3226aaea6
LapereIlse <ilse.lapere@student.howest.be>
2019-03-26 12:19:22 +0100

    Labo 26/3

--- src/script/app.js
+++ src/script/app.js
@@ -1,3 +1,30 @@
 (function() {
 	console.log('💧', 'https://www.youtube.com/watch?v=ARC1w1WWxGY');
+
+	const options = {
+		// De eenheden van je doel
+		units: 'ml',
+		// De hoeveelheid van het dagelijkse doel.
+		dailyGoal: 1539,
+		// Of je met localStorage of een API wil werken
+		mode:'local',
+		// Een object met classes voor:
+		domRefs: {
+			// het huidige percentage.
+			percentage: 'js-amount',
+			// De tijdstippen 
+			timeStampHolder: 'js-time-stamps',
+			// De 'voeg toe' buton
+			addButton: 'js-log',
+			currentGoal: 'js-goal',
+			currentUnits: 'js-units'
+		},
+		afterUpdate: function(newPercentage) {
+			console.log('It has been updated!');
+		}
+	};
+
+	document.addEventListener('DOMContentLoaded', () => {
+		new ProgressTracker(options);
+	});
 })();
